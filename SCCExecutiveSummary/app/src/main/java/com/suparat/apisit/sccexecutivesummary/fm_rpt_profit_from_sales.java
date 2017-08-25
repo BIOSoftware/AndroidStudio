14823dee27369e321049a5554be092e1e636e2b3
BIOSoftware <Aoh.Mail@GMail.com>
2017-08-25 18:03:29 +0700

    Add
    Add

--- SCCExecutiveSummary/app/src/main/java/com/suparat/apisit/sccexecutivesummary/fm_rpt_profit_from_sales.java
+++ SCCExecutiveSummary/app/src/main/java/com/suparat/apisit/sccexecutivesummary/fm_rpt_profit_from_sales.java
@@ -1,13 +1,21 @@
 package com.suparat.apisit.sccexecutivesummary;
 
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.AxisBase;
@@ -25,13 +33,30 @@ import com.suparat.apisit.sccexecutivesummary.model.TestData;
 
 
 import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Random;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class fm_rpt_profit_from_sales extends Fragment {
+public class fm_rpt_profit_from_sales extends Fragment  {
+
+    private static EditText etxt_Month;
+    private EditText etxt_Year;
+
+    private DatePickerDialog dialogMonth;
+    private DatePickerDialog dialogYear;
+    private SimpleDateFormat dateFormatter;
+
+    private View v;
+
+    private int mYear = 2013;
+    private int mMonth = 5;
+    private int mDay = 30;
 
 
     public fm_rpt_profit_from_sales() {
@@ -44,37 +69,66 @@ public class fm_rpt_profit_from_sales extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        View v = inflater.inflate(R.layout.fragment_fm_rpt_profit_from_sales, container, false);
+        v = inflater.inflate(R.layout.fragment_fm_rpt_profit_from_sales, container, false);
+
+
+        // Set BarChart
+
         BarChart barChart = (BarChart) v.findViewById(R.id.chart_rpt_profit_from_sales);
-
-
-
         //BarChart barChart = (BarChart)container.findViewById(R.id.chart_rpt_profit_from_sales);
         final ArrayList<TestData> testDatas = TestData.getDataTest(10);
 
-        final ArrayList<BarEntry> barEntry = new ArrayList<>();
+        final ArrayList<BarEntry> barEntryScore = new ArrayList<>();
+        final ArrayList<BarEntry> barEntryGrad = new ArrayList<>();
         int index = 0;
         for (TestData testData : testDatas) {
             float fScore = testData.getScore();
             String fDesc = testData.getName();
+            float fGrade = testData.getGrad();
 
-            barEntry.add(new BarEntry(index,fScore));
+            barEntryScore.add(new BarEntry(index,fScore));
+            barEntryGrad.add(new BarEntry(index,fGrade));
+
 
             index++;
         }
 
 
-        BarDataSet dataset = new BarDataSet(barEntry, "#");
-        dataset.setValueTextSize(8);
-        dataset.setColors(ColorTemplate.COLORFUL_COLORS); // set the color
+        BarDataSet datasetScore = new BarDataSet(barEntryScore, "คะแนน");
+        BarDataSet datasetGrade = new BarDataSet(barEntryGrad, "เกรด");
 
+        datasetScore.setValueTextSize(8);
+        datasetGrade.setValueTextSize(8);
+
+        List<Integer> fColorList = new ArrayList<Integer>();
+
+
+
+        for (int i=0 ;i< testDatas.size();i++){
+            Random rnd = new Random();
+
+            int fColor = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+            fColorList.add(fColor);
+        }
+
+        int fColorSet = getResources().getColor(R.color.colorChart01);
+        datasetScore.setColors(fColorSet);
+        fColorSet = getResources().getColor(R.color.colorChart02);
+        datasetGrade.setColors(fColorSet);
 
 
         ArrayList<IBarDataSet> dataSets = new ArrayList<IBarDataSet>();
-        dataSets.add(dataset);
-        BarData data = new BarData(dataSets);
+
+
+        dataSets.add(datasetScore);
+        dataSets.add(datasetGrade);
+
+        BarData data = new BarData(datasetScore,datasetGrade);
+
+
 
         barChart.setData(data);
+
 
         barChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
         barChart.getXAxis().setLabelRotationAngle(80);
@@ -107,10 +161,47 @@ public class fm_rpt_profit_from_sales extends Fragment {
 
 
 
+        float groupSpace = 0.04f;
+        float barSpace = 0.02f; // x2 dataset
+        float barWidth = 0.46f; // x2 dataset
 
 
-        //return inflater.inflate(R.layout.fragment_fm_rpt_profit_from_sales, container, false);
+
+        barChart.getBarData().setBarWidth(barWidth);
+        barChart.groupBars(0, groupSpace, barSpace);
+
+
+        etxt_Month = (EditText)v.findViewById(R.id.etxt_Month);
+        etxt_Month.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment newFragment = new DatePickerFragment();
+                newFragment.show(getActivity().getSupportFragmentManager(), "datePicker");
+            }
+        });
+
         return v;
+    }
+
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+            DatePickerDialog dialog = new DatePickerDialog(getActivity(), this, year, month, day);
+            dialog.getDatePicker().setMaxDate(c.getTimeInMillis());
+            return  dialog;
+        }
+
+        public  void onDateSet(DatePicker view, int year, int month, int day) {
+
+        }
+
     }
 
 
--- SCCExecutiveSummary/app/src/main/java/com/suparat/apisit/sccexecutivesummary/model/TestData.java
+++ SCCExecutiveSummary/app/src/main/java/com/suparat/apisit/sccexecutivesummary/model/TestData.java
@@ -1,6 +1,7 @@
 package com.suparat.apisit.sccexecutivesummary.model;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by Aoh on 8/24/2017 AD.
@@ -8,10 +9,20 @@ import java.util.ArrayList;
 
 public class TestData {
     float score;
+    float grad;
     String name;
-    public TestData(String aName,float aScore){
+    public TestData(String aName,float aScore,float aGrad){
         this.name = aName;
         this.score = aScore;
+        this.grad = aGrad;
+    }
+
+    public float getGrad() {
+        return grad;
+    }
+
+    public void setGrad(float grad) {
+        this.grad = grad;
     }
 
     public float getScore() {
@@ -32,10 +43,14 @@ public class TestData {
 
     public static ArrayList<TestData> getDataTest(int size){
         ArrayList<TestData> testDatas = new ArrayList<>();
+        Random fRandom =  new Random();
+
         for (int i = 0; i < size; i++) {
             //testDatas.add(new TestData("Android v"+i, (float) Math.random() * 100));
             int iCount = i + 1;
-            testDatas.add(new TestData("Android v"+iCount,iCount) );
+
+
+            testDatas.add(new TestData("Android v"+iCount,iCount,fRandom.nextInt(4) ) );
         }
         return testDatas;
     }
